aea6826561461d997be8f480351b54907a74d855
AH400796 <106314691+AH400796@users.noreply.github.com>
2022-08-27 20:01:11 +0000

    Deploying to gh-pages from @ AH400796/fson60-group-project-21@0c4831d33ec277f230217f94c9ef8460babb91c6 🚀

--- index.bceac1de.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},scrollbar:{el:".swiper-scrollbar"}});
-//# sourceMappingURL=index.bceac1de.js.map
