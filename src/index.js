6cc5481dcf4b2b3bcff441bba068fd0adafc8075
AH400796 <A.Hokhman@gmail.com>
2022-08-28 22:28:19 +0300

    anim

--- src/index.js
+++ src/index.js
@@ -23,11 +23,16 @@ const swiper = new Swiper('.swiper', {
     discount: document.querySelector('[data-discount]'),
   };
 
+  let disc = ['-5%', '-10%', '-15%', '-20%', '-25%'];
+  let rand = Math.floor(Math.random() * disc.length);
+  disc = disc[rand];
+
   refs.logo.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.logo.classList.toggle('animated');
     refs.discount.classList.toggle('animated');
+    document.getElementById('disc').innerHTML = disc;
   }
 })();
 
