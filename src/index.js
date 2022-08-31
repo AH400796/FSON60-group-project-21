17ec2b8d74053b1547bd4c0e9b10278025e380b5
AH400796 <A.Hokhman@gmail.com>
2022-08-31 14:39:42 +0300

    finaly?

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './js/modal-open.js';
 import './js/modal-2';
 import './js/swiper';
 import './js/animation';
-import './js/checkDish';
+import './js/check-dish';
 import './js/fixed-header';
 import './js/phone-mask';
 import './js/transparentHeader';
